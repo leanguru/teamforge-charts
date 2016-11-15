17f2018b0900df72b05d960caac9b72b74af387d
christiangut <christian@operational-research.com>
2016-11-15 20:00:09 -0200

    Added ui-grid also to the plain version

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 var restheart_config = {
-    base_url: "http://<HOST>:<PORT>/<MONGO_DB>/",
-    username: "<USERNAME>",
-    password: "<USERNAME"
+    base_url: "http://teamforge-charts.leanguru.pro:8080/cfdtest/",
+    username: "cfdtest",
+    password: "CapSSA113"
 }
 
 var epu_config_default = {
     date_from: (new Date(new Date().getTime() - ((new Date().getDay())-1) * 24 * 60 * 60 * 1000)).toISOString().substr(0, 10),
     date_until: (new Date(new Date().getTime() + (7-(new Date().getDay())) * 24 * 60 * 60 * 1000)).toISOString().substr(0, 10),
-    trackers: "<TRACKER>,<TRACKER>,<TRACKER>,...",
-    planned_date_fields: "<DATE_FIELD>,<DATE_FIELD>,<DATE_FIELD>,..."
+    trackers: "tracker88228,tracker89256,tracker96175",
+    planned_date_fields: "plannedDate,plannedDate,dataDeEntrega" 
 };
